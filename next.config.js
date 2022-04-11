ad02b43fc1bdd2d83950443cba3b5baaa0171d5b
_Zinhle <zbuhlungu1@gmail.com>
2022-04-11 18:59:49 +0200

    n

--- next.config.js
+++ next.config.js
@@ -2,6 +2,6 @@ module.exports = {
   reactStrictMode: true,
   images: {
     domains: ['assets.vercel.com', 'cloudinary.com','lh3.googleusercontent.com' ],
-    formats: ['image/webp', 'image/jpg'],
+    formats: ['image/avif', 'image/webp'],
   }
 }
